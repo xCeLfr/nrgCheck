d8dbb7d86968b95110162eb3a97b83dbdf20b871
xCeLfr <63643217+xCeLfr@users.noreply.github.com>
2020-04-20 10:31:05 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -19,14 +19,13 @@ EMAIL_TMP=~/check_message.txt
 # isStaking : perform Staking checks
 # isSynced : perfom Synchronisation tests (beta)
 # delayBlocks : number of delay blocks allowed, can be both ways (default :2)
-if [ ! -f "$ENERGI_PRM" ]
-then
+if [ ! -f "$ENERGI_PRM" ] ; then
     cat << !EOF > $ENERGI_PRM
 {
    "isMasterNode": 0,
    "isStaking": 1,
    "isSynced": 1,
-   "delayBlocks": 2
+   "delayBlocks": 3
 }
 !EOF
 fi
@@ -43,20 +42,23 @@ WALLET_LAST_DLT=$(jq -r '.balance_delta' $ENERGI_JSN)
 WALLET_LAST_CHG=$(jq -r '.balance_changed' $ENERGI_JSN)
 
 # Get Masternode Infos
-if [ $IS_MN = 1 ]
-then
+if [ $IS_MN = 1 ] ; then
         $ENERGI_CMD attach --exec "masternode.masternodeInfo(personal.listAccounts[0])" >> $ENERGI_TMP
+        MN_COLLATERAL=$($ENERGI_CMD attach --exec "web3.fromWei(masternode.masternodeInfo(personal.listAccounts[0]).collateral, 'ether')" | sed 's/\"//g')
+        echo "  nrgCollateral: $MN_COLLATERAL" >> $ENERGI_TMP
+
+        # MN_REWARD=colletaral / 1000 x 0.914
+        MN_REWARD=$(echo "scale=2;$MN_COLLATERAL / 1000 * 0.914" | bc -l)
+        echo "  mnReward: $MN_REWARD" >> $ENERGI_TMP
 fi
 
 # Get Staking infos
-if [ $IS_ST = 1 ]
-then
+if [ $IS_ST = 1 ] ; then
         $ENERGI_CMD attach --exec "miner.stakingStatus()" >> $ENERGI_TMP
 fi
 
 # Get Last generated block
-if [ $IS_SC = 1 ]
-then
+if [ $IS_SC = 1 ] ; then
         LAST_BLOCK_HEX=$(curl -s "https://explorer.energi.network/api?module=block&action=eth_block_number" | jq -r '.result' | tr [a-z] [A-Z]| cut -dX -f2)
         LAST_BLOCK_DEC=$(echo "obase=10; ibase=16;$LAST_BLOCK_HEX"| bc)
         echo "  eth_block_number: $LAST_BLOCK_DEC" >> $ENERGI_TMP
@@ -76,13 +78,13 @@ else
         echo "  balance_delta: $WALLET_LAST_DLT" >> $ENERGI_TMP
 fi
 
-# Temp file to JSON (for future web integration)
+# Temp file to JSON ( for future web integration)
 while read ligne
 do
         param=$(echo $ligne| awk -F': |,' '{print $1}')
         value=$(echo $ligne| awk -F': |,' '{print $2}')
         JO_CMD="$JO_CMD $param=$value"
-done <<<$(cat $ENERGI_TMP | egrep "isActive:|isAlive:|miner:|staking:|balance:|balance_changed:|balance_delta:|height:|totalWeight:|announcedBlock:|swFeatures:|swVersion:|eth_block_number:" | egrep -v "modules")
+done <<<$(cat $ENERGI_TMP | egrep "mnReward:|nrgCollateral:|isActive:|isAlive:|miner:|staking:|balance:|balance_changed:|balance_delta:|height:|totalWeight:|announcedBlock:|swFeatures:|swVersion:|eth_block_number:" | egrep -v "modules")
 rm $ENERGI_TMP
 jo -p $JO_CMD > $ENERGI_JSN
 
@@ -156,13 +158,15 @@ fi
 
 if [ "x$ENERGI_BAL" != "x$WALLET_LAST_BAL" ]
 then
+
         # Time since last wallet balance change
         NB_SEC=$(echo "scale=2;($(date +%s) - $WALLET_LAST_CHG)" | bc -l)
         TXT_CHANGED_SINCE=$(echo $NB_SEC | awk '{printf "%02dj %02dh %02dm %02ds\n",int($1/3600/24), int($1/3600%24), int($1/60%60), $1%60}')
 
         WALLET_DELTA=$(jq -r '.balance_delta' $ENERGI_JSN)
+        MN_REWARD=$(jq -r '.balance_delta' $ENERGI_JSN)
 
-        if [ "$WALLET_DELTA" = "0.914" ]
+        if [ "$WALLET_DELTA" = "$MN_REWARD" ]
         then
                 echo "Subject: [Energi] New MN reward!" >> $EMAIL_TMP
         elif [ "$WALLET_DELTA" = "2.28" ]
